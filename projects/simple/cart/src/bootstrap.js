d220f9e845bd1751564a0b1ef3010ced300346b7
Nikolai Tatianenko <nikolai.tatianenko@gmail.com>
2020-12-18 23:17:46 +0000

    feat: Simple App | Card | Bootstrap

---
+++ projects/simple/cart/src/bootstrap.js
@@ -0,0 +1,26 @@
+import faker from "faker";
+
+/**
+ * Mount function to render products
+ * @param {HTMLElement} el - The element to mount the products text.
+ */
+const mount = (el) => {
+  const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
+
+  el.innerHTML = cartText;
+};
+
+/**
+ * If we are in development and an element with an id of 'cart-dev' exists,
+ */
+if (process.env.NODE_ENV === "development") {
+  const el = document.querySelector("#cart-dev");
+
+  if (el) {
+    mount(el);
+  }
+}
+/**
+ * For container development or production, export the mount function.
+ */
+export { mount };
