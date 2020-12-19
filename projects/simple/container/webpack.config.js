ba4e758d852ca49e49797b169b6b007d9e364125
Nikolai Tatianenko <nikolai.tatianenko@gmail.com>
2020-12-19 03:13:46 +0000

    feat: Simple App | Container | webpack configs

---
+++ projects/simple/container/webpack.config.js
@@ -0,0 +1,29 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require(
+  'webpack/lib/container/ModuleFederationPlugin');
+const Dotenv = require('dotenv-webpack');
+module.exports = (env) => {
+  console.log('(process.env:', process.env.PORT);
+  return {
+    mode: 'development',
+    devServer: {
+      port: process.env.PORT || 3000,
+      open: true,
+    },
+    plugins: [
+      // To use .env file
+      new Dotenv({path: `./.env`}),
+      // To use Module Federation Plugin
+      new ModuleFederationPlugin({
+        name: 'container',
+        remotes: {
+          products: 'products@http://localhost:8081/remoteEntry.js',
+          cart: 'cart@http://localhost:8082/remoteEntry.js',
+        },
+      }),
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+      }),
+    ],
+  };
+};
