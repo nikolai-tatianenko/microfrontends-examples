1d21039b26eeef15c6eea92847e2ee485b8947d3
Nikolai Tatianenko <nikolai.tatianenko@gmail.com>
2020-12-27 08:15:39 +0000

    feat: Added About Page

---
+++ projects/blog/packages/landingPage/src/components/05-Pages/About.js
@@ -0,0 +1,161 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles((theme) => ({
+  '@global': {
+    ul: {
+      margin: 0,
+      padding: 0,
+      listStyle: 'none',
+    },
+  },
+  toolbar: {
+    flexWrap: 'wrap',
+  },
+  toolbarTitle: {
+    flexGrow: 1,
+  },
+  link: {
+    margin: theme.spacing(1, 1.5),
+  },
+  heroContent: {
+    padding: theme.spacing(8, 0, 6),
+  },
+  cardHeader: {
+    backgroundColor:
+      theme.palette.type === 'light'
+        ? theme.palette.grey[200]
+        : theme.palette.grey[700],
+  },
+  cardPricing: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'baseline',
+    marginBottom: theme.spacing(2),
+  },
+  footer: {
+    borderTop: `1px solid ${theme.palette.divider}`,
+    marginTop: theme.spacing(8),
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(3),
+    [theme.breakpoints.up('sm')]: {
+      paddingTop: theme.spacing(6),
+      paddingBottom: theme.spacing(6),
+    },
+  },
+}));
+
+const tiers = [
+  {
+    title: 'Free',
+    price: '0',
+    description: [
+      '10 users included',
+      '2 GB of storage',
+      'Help center access',
+      'Email support',
+    ],
+    buttonText: 'Sign up for free',
+    buttonVariant: 'outlined',
+  },
+  {
+    title: 'Pro',
+    subheader: 'Most popular',
+    price: '15',
+    description: [
+      '20 users included',
+      '10 GB of storage',
+      'Help center access',
+      'Priority email support',
+    ],
+    buttonText: 'Get started',
+    buttonVariant: 'contained',
+  },
+  {
+    title: 'Enterprise',
+    price: '30',
+    description: [
+      '50 users included',
+      '30 GB of storage',
+      'Help center access',
+      'Phone & email support',
+    ],
+    buttonText: 'Contact us',
+    buttonVariant: 'outlined',
+  },
+];
+
+const footers = [
+  {
+    title: 'Company',
+    description: ['Team', 'History', 'Contact us', 'Locations'],
+  },
+  {
+    title: 'Features',
+    description: [
+      'Cool stuff',
+      'Random feature',
+      'Team feature',
+      'Developer stuff',
+      'Another one',
+    ],
+  },
+  {
+    title: 'Resources',
+    description: [
+      'Resource',
+      'Resource name',
+      'Another resource',
+      'Final resource',
+    ],
+  },
+  {
+    title: 'Legal',
+    description: ['Privacy policy', 'Terms of use'],
+  },
+];
+const FooterUpper = () => {
+  return (
+    <Grid container spacing={4} justify="space-evenly">
+      {footers.map((footer) => (
+        <Grid item xs={6} sm={3} key={footer.title}>
+          <Typography variant="h6" color="textPrimary" gutterBottom>
+            {footer.title}
+          </Typography>
+          <ul>
+            {footer.description.map((item) => (
+              <li key={item}>
+                <Link
+                  href="projects/blog/packages/landingPage/src/components#"
+                  variant="subtitle1"
+                  color="textSecondary"
+                >
+                  {item}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
+
+export default function About () {
+  return (
+    <Layout title={'About'}>
+      <Container maxWidth="md" component="main">
+        <Grid container spacing={5} alignItems="flex-end">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+          voluptatum, quibusdam, quia, quod voluptates voluptatem quos
+          voluptatibus quae doloribus quas voluptate. Quisquam voluptatum.
+        </Grid>
+      </Container>
+    </Layout>
+
+  );
+}
