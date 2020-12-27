9f7a74623b3a218458183f4b05cfd1a54aea0cc1
Nikolai Tatianenko <nikolai.tatianenko@gmail.com>
2020-12-27 23:53:46 +0000

    refactor: update code for typescript

--- projects/simple/packages/container/configs/webpack.config.js
+++ projects/simple/packages/container/configs/webpack.config.js
@@ -1,13 +1,29 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const Dotenv = require("dotenv-webpack");
+
 module.exports = (env) => {
   console.log("(process.env:", process.env.PORT);
+
   return {
     mode: "development",
     devServer: {
       port: process.env.PORT || 3005,
       open: true,
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".jsx"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "ts-loader",
+          },
+        },
+      ],
     },
     plugins: [
       // To use .env file
--- projects/simple/packages/container/src/bootstrap.js
+++ projects/simple/packages/container/src/bootstrap.js
@@ -1,5 +1,5 @@
 import { mount as productsMount } from "products/ProductsIndex";
-import { mount as cartMount } from "cart/CartShow";
+import { mount as cartMount } from "cart/CartApp";
 
 productsMount(document.querySelector("#products-container"));
 cartMount(document.querySelector("#cart-container"));
