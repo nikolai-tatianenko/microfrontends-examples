e1e613af2e6b1d3d430be57612f91b49972f48a8
Nikolai Tatianenko <nikolai.tatianenko@gmail.com>
2021-01-02 14:55:30 +0000

    feat: Webpack configs

---
+++ projects/blog/packages/container/config/webpack.common.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-react",
+              ["@babel/preset-env", { modules: false }],
+            ],
+            plugins: ["@babel/plugin-transform-runtime"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
