7bff1a9f350e5662bf3be1b27dec059921b10671
Nikolai Tatianenko <nikolai.tatianenko@gmail.com>
2021-01-24 16:31:47 +0000

    refactor: optimize Card component

---
+++ projects/blog/packages/dashboard/src/components/Card/Card.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="custom-col-12 custom-lg-4">
+    <div class="custom-card custom-summary">
+      <span class="custom-title">{{ title }}</span>
+      <span class="custom-detail">{{ detail }}</span>
+      <span class="custom-count">{{ count }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    detail: {
+      type: String,
+      required: true,
+    },
+    count: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import 'Card';
+</style>
